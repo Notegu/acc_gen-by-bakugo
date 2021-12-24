df0103602ce2fcffd49b6802965962224db4832a
Notegu <96369209+Notegu@users.noreply.github.com>
2021-12-24 17:42:11 +0300

    Update new.py

--- new.py
+++ new.py
@@ -1,9 +1,17 @@
-key='key'
-app_name="app name"
-deviceid=""
-emaill=""
-passwordd=""
-custompwd=""
+key='8b6682d7-d9b6-40d8-9897-0c35d706940a'
+
+app_name="accountsanino"
+
+deviceid="175C341B3C8824EB06C2F17227AD9B249FF98DD9BB724ECF350D952D345B480CD9409904D893603C35"
+
+emaill="hmstfymad@gmail.com"
+
+passwordd="DARKMewa1990@"
+
+custompwd="fakebosh"
+
 private=""
-chatlink=""
-nickname=""
+
+chatlink="http://aminoapps.com/p/a6phy2"
+
+nickname="bosh"
